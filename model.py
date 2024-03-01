a36ebfd239790fe842b44bfae2850c7b23f6d325
Mike Solomon <mike@meeshkan.com>
2024-03-01 11:05:53 +0200

    transpose

--- model.py
+++ model.py
@@ -63,15 +63,15 @@ class AudioTransformer(L.LightningModule):
     def training_step(self, batch):
         x0, x1, y, rg, m = batch
         x_hat = self(x0, x1, rg, m)
-        print('x_hat', x_hat.shape, x_hat.dtype, y.shape, y.dtype)
-        loss = nn.CrossEntropyLoss()(torch.transpose(x_hat, (0,2,1)), y)
+        print("x_hat", x_hat.shape, x_hat.dtype, y.shape, y.dtype)
+        loss = nn.CrossEntropyLoss()(torch.transpose(x_hat, 1, 2), y)
         return loss
 
     def validation_step(self, batch):
         x0, x1, y, rg, m = batch
         x_hat = self(x0, x1, rg, m)
-        print('x_hat', x_hat.shape, x_hat.dtype, y.shape, y.dtype)
-        loss = nn.CrossEntropyLoss()(torch.transpose(x_hat, (0,2,1)), y)
+        print("x_hat", x_hat.shape, x_hat.dtype, y.shape, y.dtype)
+        loss = nn.CrossEntropyLoss()(torch.transpose(x_hat, 1, 2), y)
         return loss
 
     def configure_optimizers(self):
